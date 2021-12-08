f20cbdd1ac239bb22b8d3d258a255fb9a10bc34c
Feer93 <fernando.serrano444@gmail.com>
2021-12-08 02:42:03 +0100

    operator max

--- src/main/kotlin/soa/camel/Application.kt
+++ src/main/kotlin/soa/camel/Application.kt
@@ -2,16 +2,21 @@ package soa.camel
 
 import io.micrometer.core.instrument.Counter
 import io.micrometer.core.instrument.MeterRegistry
+import org.apache.camel.Exchange
+import org.apache.camel.Pattern
+import org.apache.camel.Processor
 import org.apache.camel.ProducerTemplate
 import org.apache.camel.builder.RouteBuilder
 import org.apache.camel.model.dataformat.JsonLibrary
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.expression.spel.standard.SpelCompiler.compile
 import org.springframework.stereotype.Component
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.ResponseBody
+import java.util.regex.Pattern.compile
 
 
 @SpringBootApplication
@@ -32,8 +37,10 @@ class SearchController(private val producerTemplate: ProducerTemplate) {
 
     @RequestMapping(value = ["/search"])
     @ResponseBody
-    fun search(@RequestParam("q") q: String?): Any =
+    fun search(@RequestParam("q") q: String?) {
         producerTemplate.requestBodyAndHeader(DIRECT_ROUTE, "mandalorian", "keywords", q)
+    }
+
 }
 
 @Component
@@ -43,7 +50,9 @@ class Router(meterRegistry: MeterRegistry) : RouteBuilder() {
 
     override fun configure() {
         from(DIRECT_ROUTE)
+
             .toD("twitter-search:\${header.keywords}")
+                .process(KeywordProccesor())
             .wireTap(LOG_ROUTE)
             .wireTap(COUNT_ROUTE)
 
@@ -63,6 +72,31 @@ class Router(meterRegistry: MeterRegistry) : RouteBuilder() {
             }
         }
     }
+
+class KeywordProccesor  : Processor {
+    override fun process(exchange: Exchange) {
+
+        val msg = (exchange.getIn().getHeader("keywords") as String)
+        val tokens = msg.split(" ")
+        val resultado = StringBuilder()
+
+        for (splitmsg in tokens) {
+            //max:{number} en este caso 10 y endline
+            if (splitmsg.matches("max:[0-9]+".toRegex())) {
+                val numberAttached = splitmsg.split(":").get(1) //[max,numero] nos quedamos numero
+                resultado.append("?count=").append(numberAttached)
+            }
+            else {
+                // añadimos el mensaje
+                resultado.append(splitmsg)
+            }
+        }
+
+        val answer = resultado.toString()
+        exchange.getIn().setHeader("keywords",answer)
+    }
+
+}
 
 class TaggedCounter(private val name: String, private val tagName: String, private val registry: MeterRegistry) {
     private val counters: MutableMap<String, Counter> = HashMap()
